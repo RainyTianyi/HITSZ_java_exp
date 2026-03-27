ded7ad84814c21e877813d6ec9efb80f6429e21d
RainyTianyi <1311387637@qq.com>
2026-03-27 18:53:14 +0800

    修改UML图之前，当前已将英雄机调整为单例模式

---
+++ src/edu/hitsz/aircraft/EnemyAircraft.java
@@ -0,0 +1,11 @@
+package edu.hitsz.aircraft;
+
+public abstract class EnemyAircraft extends AbstractAircraft{
+
+    public EnemyAircraft(int locationX, int locationY, int speedX, int speedY, int hp) {
+        super(locationX, locationY, speedX, speedY, hp);
+    }
+
+    // 掉落道具
+    public abstract void generateItem();
+}
