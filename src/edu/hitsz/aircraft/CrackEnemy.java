6bfdd458fdf762617a7ba31a8841f3c1beb6aa38
RainyTianyi <1311387637@qq.com>
2026-04-01 19:31:49 +0800

    删除抽象敌机类两个无用的bool量。暂时不把各个敌机具体类中关于射击方式、子弹伤害等部分，提升到抽象类中并将修改权交给工厂。这部分规则暂时封装在敌机具体类中。

--- src/edu/hitsz/aircraft/CrackEnemy.java
+++ src/edu/hitsz/aircraft/CrackEnemy.java
@@ -12,13 +12,13 @@ import java.util.List;
 public class CrackEnemy extends EnemyAircraft{
 
     // 默认每次发射两枚子弹
-    private int shootNum = 2;
+    private final int shootNum = 2;
 
-    //子弹威力
-    private int power = 30;
+    // 子弹威力
+    private final int power = 30;
 
-    //子弹射击方向 (向上发射：-1，向下发射：1)
-    private int direction = 1;
+    // 子弹射击方向 (向上发射：-1，向下发射：1)
+    private final int direction = 1;
 
     // 精锐敌机随机发射道具的概率
     // TODO 改UML图
